40f3a68a18454b60c08b7a92d9ed242b17be8193
BK <96270359+BK200211@users.noreply.github.com>
2025-03-03 09:02:33 +0100

    Update classement_plateformes.py

--- classement_plateformes.py
+++ classement_plateformes.py
@@ -2,16 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Chargement des données
 df = pd.read_csv("vgsales.csv")
 
-# Vérifier des données
 print(df.head())
 
-# Top 10 des plateformes avec le plus de ventes
 top_platforms = df.groupby('Platform')['Global_Sales'].sum().sort_values(ascending=False).head(10)
 
-# Géneration du graphique
 plt.figure(figsize=(12,6))
 sns.barplot(x=top_platforms.index, y=top_platforms.values, palette="viridis")
 plt.xlabel("Plateformes")
