a44413322c2653f600a10ebd48f85427d1eed8ef
BK <96270359+BK200211@users.noreply.github.com>
2025-03-03 09:02:20 +0100

    Update analyze_data.py

--- analyze_data.py
+++ analyze_data.py
@@ -2,10 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("vgsales.csv")
 
-# Aperçu des données
 print(df.head())
 
-# Top 5 jeux les plus vendus
 top_games = df[['Name', 'Global_Sales']].sort_values(by='Global_Sales', ascending=False).head()
 print("\n🎮 Top 5 jeux les plus vendus :")
 print(top_games)
